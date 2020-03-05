0688822f044198340d8e4b894abc25ee39a7d01d
MartinsOnuoha <martinsvictor.nd@gmail.com>
2020-03-05 22:02:09 +0100

    remove redundant variable declarations

--- Multiples3and5/Sources/Multiples3and5/main.swift
+++ Multiples3and5/Sources/Multiples3and5/main.swift
@@ -2,10 +2,7 @@ func multiples(limit: Int) -> Int {
     var total: Int = 0;
     
     for i in 1...limit {
-        let isMultipleOf3 = i % 3 == 0
-        let isMultipleOf5 = i % 5 == 0
-        
-        if ((isMultipleOf3) || (isMultipleOf5)) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
             total += i;
         }
     }
